761ede4c110cd121f80c1a230deb8f1e06fe1b7d
Bobby White <bobby.white@bobbys-mbp.lan>
2021-10-08 14:20:07 -0400

    Externalize username

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Exit on error!
 set -euxo pipefail
+
+function prop {
+    grep "${1}" project.properties|cut -d'=' -f2
+}
 #create scratch org
-sfdx force:org:create -f config/project-scratch-def.json -s -a LOGGER username=admin@logger.org --durationdays 28
+sfdx force:org:create -f config/project-scratch-def.json -s -a LOGGER username="$(prop 'user.admin' )" --durationdays 28
 
 
 sfdx force:org:open
